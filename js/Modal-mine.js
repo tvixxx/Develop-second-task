a526639d6bf9b440d66d9de4929285a871446717
tvixxx <tvix2000@yandex.ru>
2015-12-08 15:50:09 +0400

    Added modal windows,animate,validation,reset and persent width

--- js/Modal-mine.js
+++ js/Modal-mine.js
@@ -1,3 +1,52 @@
 $(document).ready(function(){
+    var butLink = $(".btn-link");
+    var modalPopup = $(".modal-block").addClass("hide");
+    var closeModalPopup = $(".close-modal-block");
+
+    var modalSuccess = $(".modal-block-success");
+    var butSend = $(".btn-send");
+    var resetForm = $("#form-reset-login");
+    var closeSuccessBtn = $(".close-modal-btn");
+
+    var form = $(".login-form");
+    var email = $("[name=email-id]")
+    var closeEmailBtn = $(".close-modal-btn-email");
+    var errorEmailBlock = $(".modal-block-error-email");
+
+
+    butLink.click(function(event){
+    	event.preventDefault();
+    	modalPopup.show(400);
+    });
+
+    closeModalPopup.click(function(event){
+    	event.preventDefault();
+    	modalPopup.hide(400);
+    })
+
+	butSend.click(function(event){
+		event.preventDefault();
+			if (!(email.value == " ")) {
+    		errorEmailBlock.show(400);
+    		return;
+    		};
+		$("#form-reset-login").get(0).reset();
+		modalSuccess.animate({
+    		'opacity' : 'show'
+    	},200);
+	});
+
+ 	closeSuccessBtn.click(function(event){
+ 		event.preventDefault();
+ 			modalSuccess.hide(400);
+    			modalPopup.hide(400);
+    		});
+
+	closeEmailBtn.click(function(event){
+		event.preventDefault();
+		errorEmailBlock.hide(400);
+	});
+
 
 });
+
